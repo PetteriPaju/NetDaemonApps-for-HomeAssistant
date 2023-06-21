a4f954f4c997120af2ed0788cf74619309c1c2a1
Petteri Paju <paju.petteri@luukku.com>
2023-06-21 12:47:19 +0300

    fixed auto turn on

--- apps/AutoTurnOffs.cs
+++ apps/AutoTurnOffs.cs
@@ -13,13 +13,6 @@ namespace NetDaemonApps.apps
 
             var _myEntities = new Entities(ha);
 
-            _myEntities.Switch.PcMultipowermeterMonitors.StateChanges().Subscribe(_ => {
-                if (_myEntities.Switch.PcMultipowermeterMonitors.IsOn())
-                    _myEntities.Switch.OutsideTemperatureMeterSwitch.TurnOn();
-                else
-                    _myEntities.Switch.OutsideTemperatureMeterSwitch.TurnOff();
-            });
-
             _myEntities.Sensor.PcDisplayDisplayCount.StateChanges().Subscribe(_ => { _myEntities.Button.PcResetbrigtness.Press(); });
             _myEntities.Sensor.PcDisplayDisplay1Resolution.StateChanges().Subscribe(_ => { _myEntities.Button.PcResetbrigtness.Press(); });
 
@@ -36,19 +29,19 @@ namespace NetDaemonApps.apps
             _myEntities.Switch.BedMultiPlugL2.StateChanges().WhenStateIsFor(x => x?.State == "on", TimeSpan.FromHours(1))
                .Subscribe(x => { _myEntities.Switch.BedMultiPlugL2.TurnOff(); });
 
-            _myEntities.Switch.PcPlug.StateChanges().Where(x => x.New?.State == "on")
+            _myEntities.Switch.PcPlug.StateChanges().Where(x => x.New?.State == "on" && x.Old?.State == "off")
                 .Subscribe(_ => {
 
                  _myEntities.Light.PcMultipowermeterL2.TurnOn();
                     AgaraCube_LivingRoom.BrightLightTurnedOnByPC = true;
                 _myEntities.Switch.PcMultipowermeterMonitors.TurnOn();
                 _myEntities.Switch.FanPlug.TurnOn();
-                _myEntities.Scene.SwitchUsbPc.TurnOn();
+                _myEntities.Scene.SwitchUsbLaptop.TurnOn();
 
             });
 
        
-            _myEntities.Switch.PcPlug.StateChanges().Where(x => x.New?.State == "off")
+            _myEntities.Switch.PcPlug.StateChanges().Where(x => x.New?.State == "off" && x.Old?.State == "on")
                 .Subscribe(_ => {
 
 
@@ -56,9 +49,8 @@ namespace NetDaemonApps.apps
                 {
                         _myEntities.Light.PcMultipowermeterL2.TurnOff();
                         _myEntities.Switch.PcMultipowermeterMonitors.TurnOff();
-                  
                 }
-                _myEntities.Scene.SwitchUsbLaptop.TurnOn();
+                _myEntities.Scene.SwitchUsbPc.TurnOn();
 
             });
 
