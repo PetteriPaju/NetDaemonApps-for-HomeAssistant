6e8dffc5377a834957f05b08c96eaf38dd71c9e4
Zatman <Zatman@DESKTOP-K24V8NP>
2025-03-05 07:29:59 +0200

    walkingpad update

--- apps/Hue Switches/Livingroom Switch.cs
+++ apps/Hue Switches/Livingroom Switch.cs
@@ -17,37 +17,26 @@ namespace NetDaemonApps.apps.Hue_Switches
       
         public Livingroom_Switch() : base() {
 
-            A0Gbl._myEntities.Sensor.ModemAutoOnPlugPower.StateChanges().WhenStateIsFor(x => x?.State < 19, TimeSpan.FromMinutes(30), A0Gbl._myScheduler).Subscribe(x => {
-
-                DeActivateRunningPad();
-                SafeTurnOff();
-
-            });
-
-            A0Gbl._myEntities.Switch.ModemAutoOnPlug.StateChanges().Where(x => x.New.IsOn()).Subscribe(x => { ActivateRunningPad(); });
-
-                A0Gbl._myEntities.Sensor.PcWalkingpadActive.StateChanges().Where(x => x.New.State == "1").Subscribe(x => {
+                A0Gbl._myEntities.Sensor.PcWalkingpadActive2.StateChanges().Where(x => x.New.State == "1").Subscribe(x => {
                     speedIncrementRoutine?.Dispose();
                     speedIncrementRoutine = A0Gbl._myScheduler.RunEvery(TimeSpan.FromMinutes(5),(DateTime.Now+TimeSpan.FromMinutes(5)), () => {
-
-
                         A0Gbl._myEntities.Button.PcWalkingpadSpeedupone.Press();
                     });
                 });
 
-            A0Gbl._myEntities.Sensor.PcWalkingpadActive.StateChanges().Where(x => x.New.State != "1").Subscribe(x => {
+            A0Gbl._myEntities.Sensor.PcWalkingpadActive2.StateChanges().Where(x => x.New.State != "1").Subscribe(x => {
                 speedIncrementRoutine?.Dispose();
             });
+
+
+            A0Gbl._myEntities.BinarySensor.WalkingpadContactSensorContact.StateChanges().Where(x => x.New.IsOff()).Subscribe(x => { ActivateRunningPad(); });
+            A0Gbl._myEntities.BinarySensor.WalkingpadContactSensorContact.StateChanges().Where(x => x.New.IsOn()).Subscribe(x => { DeActivateRunningPad(); });
+
         }
   
-        protected void SafeTurnOff()
-        {
-            if(A0Gbl._myEntities.Sensor.ModemAutoOnPlugPower.State > 15)
-                A0Gbl._myEntities.Switch.ModemAutoOnPlug.TurnOff();
-        }
         protected bool hasProcess()
         {
-            return A0Gbl._myEntities.Sensor.PcWalkingpadActive.State == "1";
+            return A0Gbl._myEntities.Sensor.PcWalkingpadActive2.State == "1";
         }
         protected SwitchEntity runnerSwitch { get { return A0Gbl._myEntities.Switch.ModemAutoOnPlug;  } }
         protected void ActivateRunningPad()
@@ -61,25 +50,20 @@ namespace NetDaemonApps.apps.Hue_Switches
 
             if (!hasProcess())
             {
-            A0Gbl._myScheduler.Schedule(TimeSpan.FromSeconds(wasPlugOn ? 0 : 5), A0Gbl._myEntities.Button.PcStartrunningpadprocess.Press);
+            A0Gbl._myScheduler.Schedule(TimeSpan.FromSeconds(wasPlugOn ? 0 : 5), A0Gbl._myEntities.Button.PcWalkingpadprocessrun.Press);
             }
- 
-
+            else
+            {
+                A0Gbl._myEntities.Button.PcWalkingpadprocessrun.Press();    
+            }
         }
 
         protected void DeActivateRunningPad()
         {
-            bool wasRunning = false;
-            if(A0Gbl._myEntities.Sensor.ModemAutoOnPlugPower.State > 15)
-            {
-                ToggleRunner();
-                wasRunning = true;
-            }
 
-            if (hasProcess())
-            {
-                A0Gbl._myScheduler.Schedule(TimeSpan.FromSeconds(wasRunning ? 30 : 0), A0Gbl._myEntities.Button.PcStopwalkingpadprocess.Press);
-            }
+            runnerSwitch.TurnOff();
+            A0Gbl._myEntities.Button.PcWalkingpadprocesskill.Press();
+
 
         }
 
@@ -101,16 +85,7 @@ namespace NetDaemonApps.apps.Hue_Switches
         protected override void OnPowerRelease()
         {
             base.OnPowerPress();
-            if (runnerSwitch.IsOff() || A0Gbl._myEntities.Sensor.PcWalkingpadActive.State != "1")
-            {
-                ActivateRunningPad();
-                return;
-            }
-            else
-            {
-                ToggleRunner();
-            }
-            
+            ToggleRunner();
         }
   
         protected override void OnPowerHoldRelease()
@@ -164,17 +139,7 @@ namespace NetDaemonApps.apps.Hue_Switches
         protected override void OnHueRelease()
         {
             base.OnHueRelease();
-
-            if (hasProcess())
-            {
-                DeActivateRunningPad();
-            }
-            else
-            {
-                ActivateRunningPad();
-            }
-
-         
+        
         }
 
 
